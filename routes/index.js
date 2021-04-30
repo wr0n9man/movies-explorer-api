02c08461c98cfa24d286caa898d1cc63aa9aa25d
wr0n9man <rafigov2017@yandex.ru>
2021-04-30 10:34:05 +0400

    fixed

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,14 @@ const auth = require('../middlewares/auth');
 const user = require('./users');
 const movie = require('./movies');
 
-router.post('/api/signin', celebrate({
+router.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(6),
   }),
 }), login);
 
-router.post('/api/signup', celebrate({
+router.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email(),
@@ -21,8 +21,8 @@ router.post('/api/signup', celebrate({
   }),
 }), createUser);
 
-router.use('/api/', auth, movie);
-router.use('/api/', auth, user);
+router.use('/', auth, movie);
+router.use('/', auth, user);
 
 router.get('*',function(req, res, next){
   next(new NotFoundError('Страницы не существует'));
